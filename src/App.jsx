d81f9e449bf198457e535b2409aa66e79fe4c929
danchooalex <danchooalexandar@gmail.com>
2024-09-27 13:08:18 +0200

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,12 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="product" element={<Product />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>LIST</p>} />
           <Route path="cities" element={<p>List of cities</p>} />
           <Route path="countries" element={<p>List of Countries</p>} />
           <Route path="form" element={<p>Form</p>} />
